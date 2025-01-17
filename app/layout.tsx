c2cfdb60c3c8425b425bef280dcb44ffe40d6099
dhien <hienronsard@gmail.com>
2025-01-17 19:08:32 +0000

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 
 import "@/styles/globals.css";
 import type { Metadata } from "next";
-import {NextUIProvider} from "@nextui-org/react";
+import {NextUIProvider} from "@nextui-org/react"; 
 
 export const metadata: Metadata = {
   title: "Meeting",
